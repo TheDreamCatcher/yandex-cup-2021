5863db133e9d1157d03243f2ec4a7785c610f57f
Gladkikh Yuriy <iurii.gladkikh@domru.ru>
2021-10-02 15:54:13 +0800

    c - with generator

--- c.php
+++ c.php
@@ -1,80 +1,149 @@
 <?php
 
-$inputData = file_get_contents('input.txt');
-$data = explode("\n", $inputData);
+$maxTimeOut = 2;
 
-$k = $data[0];
+while (true) {
+    $start = microtime(true);
+    generateTest();
 
-$a = explode(' ', $data[1]);
-$b = explode(' ', $data[2]);
+    $inputData = file_get_contents('input.txt');
+    $data = explode("\n", $inputData);
 
-$totalAmountOfBalls = array_sum($a);
-$totalBoxes = $totalAmountOfBalls;
+    $k = $data[0];
 
-for ($i = 0; $i < $k; ++$i) {
-    if ($b[$i] != 0) {
-        $totalBoxes = min(
-            $totalBoxes,
-            (int) ($a[$i] / $b[$i])
-        );
+    $a = explode(' ', $data[1]);
+    $b = explode(' ', $data[2]);
+
+    $totalAmountOfBalls = array_sum($a);
+    $totalBoxes = $totalAmountOfBalls;
+
+    for ($i = 0; $i < $k; ++$i) {
+        if ($b[$i] != 0) {
+            $totalBoxes = min(
+                $totalBoxes,
+                (int) ($a[$i] / $b[$i])
+            );
+        }
     }
-}
-
-if ($totalBoxes < 1) {
-    echo '0';
-    return;
-}
-
-while ($totalAmountOfBalls / $totalBoxes != (int) ($totalAmountOfBalls / $totalBoxes)) {
-    $totalBoxes--;
 
     if ($totalBoxes < 1) {
         echo '0';
-        return;
+        die();
     }
-}
 
-$ballsInBox = $totalAmountOfBalls / $totalBoxes;
+    while ($totalAmountOfBalls / $totalBoxes != (int) ($totalAmountOfBalls / $totalBoxes)) {
+        $totalBoxes--;
 
-if($ballsInBox<0){
-    echo 0;
-    return;
-}
+        if ($totalBoxes < 1) {
+            echo '1';
+            die();
+        }
+    }
 
-$freeBalls = [];
-for ($i = 0; $i < $k; ++$i) {
-    $freeBalls[$i] = $a[$i] - $b[$i] * $totalBoxes;
-}
+    $ballsInBox = $totalAmountOfBalls / $totalBoxes;
 
-$freeColor = 0;
+    $freeBalls = [];
 
-echo $totalBoxes . ' ' . $ballsInBox . PHP_EOL;
+    for ($i = 0; $i < $k; ++$i) {
+        $freeBalls[$i] = $a[$i] - $b[$i] * $totalBoxes;
+    }
 
-for ($box = 0; $box < $totalBoxes; $box++) {
-    $needToFeel = $ballsInBox;
+    $freeColor = 0;
 
-    for ($color = 0; $color < $k; $color++) {
-        if ($b[$color] > 0) {
-            $needToFeel -= $b[$color];
-            for ($j = 0; $j < $b[$color]; $j++) {
-                echo ($color + 1) . ' ';
+    $result = $totalBoxes . ' ' . $ballsInBox . PHP_EOL;
+
+    for ($box = 0; $box < $totalBoxes; $box++) {
+        $needToFeel = $ballsInBox;
+
+        for ($color = 0; $color < $k; $color++) {
+            if ($b[$color] > 0) {
+                $needToFeel -= $b[$color];
+                for ($j = 0; $j < $b[$color]; $j++) {
+                    $result .= ($color + 1) . ' ';
+                }
             }
         }
-    }
 
-    while ($needToFeel > 0) {
-        if ($freeBalls[$freeColor] > 0) {
-            echo ($freeColor + 1) . ' ';
-            $freeBalls[$freeColor]--;
-            $needToFeel--;
-        } else {
-            $freeColor++;
+        while ($needToFeel > 0) {
+
+            $end = microtime(true);
+
+            if ($end - $start > $maxTimeOut) {
+                echo 'timelimit 0';
+                die();
+            }
+
+            if ($freeBalls[$freeColor] > 0) {
+                $result .= ($freeColor + 1) . ' ';
+                $freeBalls[$freeColor]--;
+                $needToFeel--;
+            } else {
+                $freeColor++;
+            }
+
+            if ($freeColor >= $k) {
+                echo '2';
+                die();
+
+            }
         }
 
-        if ($freeColor >= $k) {
-            return;
+        $end = microtime(true);
+
+        if ($end - $start > $maxTimeOut) {
+            echo 'timelimit 1';
+            die();
         }
+
+        $result .= PHP_EOL;
     }
 
-    echo PHP_EOL;
+    if (validateResult($result)) {
+        if ($end - $start > $maxTimeOut) {
+            echo 'timelimit 1';
+            die();
+        }
+
+        echo 'Valid ' . number_format($end - $start, 3) . ' ' . date('H:i:s') . PHP_EOL;
+    } else {
+        echo 'FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
+        die();
+    }
+}
+
+function validateResult($result)
+{
+    $data = explode("\n", trim($result));
+    [$boxCount, $ballsCount] = explode(' ', trim($data[0]));
+
+    if ($boxCount != count($data) - 1) {
+        return false;
+    }
+
+    for ($i = 1; $i <= $boxCount; ++$i) {
+        if ($ballsCount != count(explode(' ', trim($data[$i])))) {
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+function generateTest()
+{
+    $k = random_int(1, 100000);
+    $a = [];
+    $b = [];
+    $total = 100000;
+
+    for ($i = 0; $i < $k; ++$i) {
+        $a[$i] = random_int(1, max(1, min($total, 100000)));
+        $total -= $a[$i];
+        $b[$i] = random_int(0, $a[$i]);
+    }
+
+    file_put_contents('input.txt',
+        "{$k}\n" . implode(' ', $a) . "\n" . implode(' ', $b)
+    );
 }
